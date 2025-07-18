b104264c5e87a89e20d61a1ee7fb072f64f9aee0
Maxwell Jones <maxjones2001@hotmail.com>
2025-07-18 17:59:58 -0400

    max update

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            max's app
+           max page <code>app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
